2c2dfd665b087a24330bc1a55c83c943d770f96f
aadgym <aad_gym@outlook.com>
2022-06-12 22:05:44 +0700

    callback challenge

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs');
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,56 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+
+// const bacaData = (fnCallback) => {
+//   const inputFile = [file1,file2,file3];
+//   let outputnya = inputFile.map((file) => {
+//       fs.readFile(file, (err,data)=> {
+//           if (err) throw err;
+//           let content = JSON.parse(data);
+//           let sentence = null;
+//           if (content && content.message){
+//             sentence = content.message
+//           }
+//           if (content[0] && content[0].message){
+//             sentence = content[0].message
+//           }
+//           if (content[0] && content[0].data && content[0].data.message){
+//             sentence = content[0].data.message;
+//           }
+//           let word = sentence.split(' ')[1];
+//           return word;
+//       })
+//   });
+//   fnCallback(null,outputnya);
+// }
+
+const bacaData = (fnCallback) => {
+	let arrOutput = [];
+	fs.readFile(file1, (err,data) => {
+		if (err) throw err;
+		let content = JSON.parse(data);
+		let sentence = content.message;
+		let word = sentence.split(' ')[1];
+		arrOutput.push(word);
+    fs.readFile(file2, (err,data) => {
+      if (err) throw err;
+      let content = JSON.parse(data);
+      let sentence = content[0].message;
+      let word = sentence.split(' ')[1];
+      arrOutput.push(word);
+      fs.readFile(file3, (err,data) => {
+        if (err) throw err;
+        let content = JSON.parse(data);
+        let sentence = content[0].data.message;
+        let word = sentence.split(' ')[1];
+        arrOutput.push(word);
+        fnCallback(null,arrOutput);
+      })
+    })
+	});
+
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
